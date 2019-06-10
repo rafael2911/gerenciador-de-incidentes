333f5577d354ec913bff0f7201c195afa36c591f
Rafael Carvalho <crcarvalho@outlook.com>
2019-06-10 18:02:54 -0300

    Criação do projeto

---
+++ src/main/java/br/com/crcarvalho/incidentes/GerenciadorDeIncidentesApplication.java
@@ -0,0 +1,13 @@
+package br.com.crcarvalho.incidentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeIncidentesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeIncidentesApplication.class, args);
+	}
+
+}
