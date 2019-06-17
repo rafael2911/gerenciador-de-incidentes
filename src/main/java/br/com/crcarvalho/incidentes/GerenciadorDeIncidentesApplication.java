3b0ace936b74072a7fab215c1adadf02e11ccc07
Rafael Carvalho <crcarvalho@outlook.com>
2019-06-17 15:45:02 -0300

    Configurando a exibição de datas na view

--- src/main/java/br/com/crcarvalho/incidentes/GerenciadorDeIncidentesApplication.java
+++ src/main/java/br/com/crcarvalho/incidentes/GerenciadorDeIncidentesApplication.java
@@ -2,12 +2,19 @@ package br.com.crcarvalho.incidentes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 @SpringBootApplication
 public class GerenciadorDeIncidentesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciadorDeIncidentesApplication.class, args);
+	}
+	
+	@Bean
+	public Java8TimeDialect java8TimeDialect() {
+		return new Java8TimeDialect();
 	}
 
 }
